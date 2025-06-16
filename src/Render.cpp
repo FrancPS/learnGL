fe38b927a7521b791ec92cc251683aa56e4e5764
FrancPS <fportasol@gmail.com>
2025-06-16 23:47:39 +0200

    Variable Green Triangle

--- src/Render.cpp
+++ src/Render.cpp
@@ -112,6 +112,7 @@ void Render::initShaders()
     // SHADER PROGRAM
     shaderProgramOrange = CreateProgram(vertexShader, fragmentShaderOrange);
     shaderProgramYellow = CreateProgram(vertexShader, fragmentShaderYellow);
+    shaderProgramVariableGreen = CreateProgram(vertexShader, fragmentShaderVariableGreen);
 }
 
 void Render::Update()
@@ -130,7 +131,13 @@ void Render::Update()
         glDrawArrays(GL_TRIANGLES, 0, 3);
         //glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
 
-        glUseProgram(shaderProgramYellow);
+        glUseProgram(shaderProgramVariableGreen);
+
+        float timeValue = glfwGetTime();
+        float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
+        int vertexColorLocation = glGetUniformLocation(shaderProgramVariableGreen, "ourColor");
+        glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
+
         glBindVertexArray(VAOs[1]);
         glDrawArrays(GL_TRIANGLES, 0, 3);
         //glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
